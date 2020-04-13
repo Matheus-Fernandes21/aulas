8ed04267727b9fb03c5f293a81d5dc89ff07d98a
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-04-13 17:31:43 -0300

    Add files via upload

---
+++ operadores.java
@@ -0,0 +1,37 @@
+
+public class operadores {
+
+	public static void main(String[] args) {
+	
+	    int y = 16;
+		int x =20;	
+		int soma = x+y;
+		int multiplicacao = x*y;
+		int divisaoInteira = x/y;
+		int modulo = x%y;
+		int subtracao = x-y;
+		double divisaocomum = (double) x/y;
+		
+		
+		
+		
+		System.out.println("soma: "+soma);
+		System.out.println("subtracao: "+subtracao);
+	    System.out.println("multiplicacao: "+multiplicacao);
+	    System.out.println("divisao inteira: "+divisaoInteira);
+        System.out.println("divisao comum: "+divisaocomum);
+        System.out.println("modulo: "+modulo);
+        
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
